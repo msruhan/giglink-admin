a0b26bb23977d1b1307a1e91239ece08f8c3ed2c
msruhan <masruhan.khotibb@gmail.com>
2025-10-20 15:33:16 +0700

    Add upload license component and implement license submission logic

--- src/pages/explore-item/upload-license.vue
+++ src/pages/explore-item/upload-license.vue
@@ -18,7 +18,7 @@ import sidebar from '@/components/sidebar/sidebar.vue';
 import headers from '@/components/header/header.vue';
 import footers from '@/components/footer/footer.vue';
 import switcher from '@/components/switcher.vue';
-import uploadwork from '@/components/upload-work.vue'
+import uploadwork from '@/components/upload-license.vue'
 
 export default {
     data() {
